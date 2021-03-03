e13974be9abfa8492a2cd71d43ce42f120cd3578
TonyNavas <tonynavas777@gmail.com>
2021-03-03 00:51:45 -0600

    Agregue la estructura donde se mostraran los datos

---
+++ Ciclo/Ciclo.php
@@ -0,0 +1,31 @@
+<?php
+if(!$_POST) {
+header('Location:index.html');
+}
+$nombre = $_POST['nombre'];
+$número = $_POST['numero'];
+$ciclo = 1;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Registro del ciclo</title>
+</head>
+<body>
+<body>
+<table>
+<thead>
+<tr>
+<th>Nombre: <?php echo $nombre ?>
+</th>
+</tr> 
+<tr><th>Numero de repeticiones:</td></th>    
+<td><?php echo $número ?></td>                          
+</tr>
+</thead>
+</table>
+</body>
+<?php       
